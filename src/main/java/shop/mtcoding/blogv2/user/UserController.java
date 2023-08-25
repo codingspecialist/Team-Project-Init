67ce6227aebf226ee5f8ddf0fb28a1d7b97953f7
MinkyeongSung <wildlegion59@gmail.com>
2023-08-25 09:54:07 +0900

    회원가입완료

--- src/main/java/shop/mtcoding/blogv2/user/UserController.java
+++ src/main/java/shop/mtcoding/blogv2/user/UserController.java
@@ -1,9 +1,23 @@
 package shop.mtcoding.blogv2.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
-  
+    // 회원가입 페이지 불러오기
+    @GetMapping("/joinForm")
+    public String joinForm(){
+        return "user/joinForm";
+    }
+
+    // 회원가입
+    @PostMapping("/join")
+    public String join(){
+        System.out.println("회원가입완료");
+        return "redirect:/login";
+    }
+
 }
