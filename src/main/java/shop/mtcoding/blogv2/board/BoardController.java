21f765dc45d7f6c513ffe2fd886ad0712f362247
Merhong <133627994+Merhong@users.noreply.github.com>
2023-08-25 10:37:55 +0900

    홈페이지(게시글목록) (#3)
    * 게시글 목록보기
    
    Co-authored-by: codingspecialist <codingspecialist@naver.com>

--- src/main/java/shop/mtcoding/blogv2/board/BoardController.java
+++ src/main/java/shop/mtcoding/blogv2/board/BoardController.java
@@ -1,16 +1,35 @@
 package shop.mtcoding.blogv2.board;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class BoardController {
 
+    @Autowired
+    private BoardService boardService;
+
+    @Autowired
+    private BoardRepository boardRepository;
+
+
+    // index(), 홈페이지
+    @GetMapping("/")
+    public String index() {
+        System.out.println("홈페이지&게시글 목록123");
+        return "index";
+    }
+
     // 게시글 작성 화면 보여주기
     @GetMapping("/board/saveForm")
     public String saveForm(){
         return "board/saveForm";
+
     }
 
     // 게시글 작성하기 기능
